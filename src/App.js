f26ed35bab8ce4495495d56a0f5824518a25085e
Felipe <felmartins1985@gmail.com>
2022-09-06 19:09:05 -0300

    feat: criado o remote de desenvolvimento

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-
 import StrangerThings from './components/StrangerThings';
+
+require('dotenv').config();
 
 function App() {
   return (
     <div className="App">
+      {process.env.REACT_APP_PD && <h4>Em desenvolvimento</h4>}
       <StrangerThings />
     </div>
   );
